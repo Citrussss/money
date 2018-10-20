e8a175ff551fcd6dd0995bfdef5aa7320051615e
bangbang <292837678@qq.com>
2018-10-20 11:59:19 +0800

    号利弊的日你

--- app/src/main/java/com/jby/money/inject/data/api/Api.java
+++ app/src/main/java/com/jby/money/inject/data/api/Api.java
@@ -1,11 +1,15 @@
 package com.jby.money.inject.data.api;
 
 
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
 /**
  * Created by arvin on 2017/11/28.
  */
 
 public interface Api {
 
-
+    @GET("/login")
+    Observable<String> login();
 }
