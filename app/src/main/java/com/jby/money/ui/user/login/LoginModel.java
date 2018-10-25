85cf8ece89660119e7062977648c4744cd6065ae
bangbang <292837678@qq.com>
2018-10-25 22:20:26 +0800

    横向滚动布局

--- app/src/main/java/com/jby/money/ui/user/login/LoginModel.java
+++ app/src/main/java/com/jby/money/ui/user/login/LoginModel.java
@@ -9,6 +9,10 @@ import com.binding.model.model.ModelView;
 import com.binding.model.model.ViewHttpModel;
 import com.binding.model.model.ViewModel;
 import com.binding.model.util.BaseUtil;
+import com.facebook.rebound.SimpleSpringListener;
+import com.facebook.rebound.Spring;
+import com.facebook.rebound.SpringConfig;
+import com.facebook.rebound.SpringSystem;
 import com.jby.money.R;
 import com.jby.money.base.arouter.ArouterUtil;
 import com.jby.money.base.rxjava.RestfulTransformer;
@@ -50,19 +54,37 @@ public class LoginModel extends ViewHttpModel<LoginActivity, ActivityLoginBindin
     public String loginBg = "http://kk.51.com/refer?url=https://mmbiz.qpic.cn/mmbiz_gif/BOeKkc9wvYe92nLf0lTPMMLNmZP3gWK5iaDTZoQWcveThSk4XfWAia7QpmArbiaX0V1NiaWF027OKPMLU8mdmiaDKEg/640?wx_fmt=gif";
     @Inject
     Api api;
+    private SpringSystem springSystem;
+    private Spring spring;
 
     @Override
     public void attachView(Bundle savedInstanceState, LoginActivity loginActivity) {
         super.attachView(savedInstanceState, loginActivity);
+//        getDataBinding().scrollView.fullScroll( View.FOCUS_RIGHT);
+        springSystem = SpringSystem.create();
+        spring = springSystem.createSpring();
+        spring.setCurrentValue(0f);
+        spring.setSpringConfig(new SpringConfig(50,3));
+        spring.addListener(new SimpleSpringListener() {
+            @Override
+            public void onSpringUpdate(Spring spring) {
+                float value = (float) spring.getCurrentValue();
+                float scale = 1-0.5f*value;
+                getDataBinding().register.setScaleX(scale);
+                getDataBinding().register.setScaleY(scale);
+            }
+        });
     }
 
     public void onLoginClick(View view) {
-//        ArouterUtil.navigation(home);
+        spring.setCurrentValue(0.5);
+        spring.setEndValue(0);
 
-        addDisposable(api.login()
-                .compose(new RestfulTransformer<>())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe((userEntity -> getDataBinding().setParams(userEntity.get(0))),FunnyToast::error));
+//        ArouterUtil.navigation(home);
+//        addDisposable(api.login()
+//                .compose(new RestfulTransformer<>())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe((userEntity -> getDataBinding().setParams(userEntity.get(0))),FunnyToast::error));
 //        ArouterUtil.navigation(home);
     }
 
